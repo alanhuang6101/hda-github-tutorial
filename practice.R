b1d442f054dc2e49121273d392d5b82b0733ae16
Shiqi Huang <sh425@login-c.cx3.hpc.ic.ac.uk>
2026-03-04 13:30:56 +0000

    Edit practice.R with vis

--- practice.R
+++ practice.R
@@ -5,3 +5,8 @@ x <- rnorm(n)
 y <- 1.5 * x + rnorm(n, mean = 0, sd = 5)
 df <- data.frame(dose = n, outcome = y)
 model <- lm (y ~x)
+# ---- visualization ----
+plot(df$x, df$y,
+     xlab = "x", ylab = "y",
+     main = "Scatter plot of y vs x")
+abline(lm(y ~ x, data = df), lwd = 2)
